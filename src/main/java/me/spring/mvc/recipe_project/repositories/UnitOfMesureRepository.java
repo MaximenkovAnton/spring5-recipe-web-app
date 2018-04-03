ae5799a288455642f7a7007bbc3ba323c68fc69c
Anton Maximenkov <maximenkovanton@gmail.com>
2018-04-04 02:26:36 +0300

    Added unit of mesure repository

---
+++ src/main/java/me/spring/mvc/recipe_project/repositories/UnitOfMesureRepository.java
@@ -0,0 +1,7 @@
+package me.spring.mvc.recipe_project.repositories;
+
+import me.spring.mvc.recipe_project.domain.UnitOfMesure;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UnitOfMesureRepository extends CrudRepository<UnitOfMesure, Long>{
+}
