9a179b9720e1f7651cf53465cad30cb712c16a91
Anton Maximenkov <maximenkovanton@gmail.com>
2018-04-04 20:08:32 +0300

    Added several constructors to ingredient to increasing speed of setting data

--- src/main/java/me/spring/mvc/recipe_project/domain/Ingredient.java
+++ src/main/java/me/spring/mvc/recipe_project/domain/Ingredient.java
@@ -1,12 +1,16 @@
 package me.spring.mvc.recipe_project.domain;
 
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
 
 import javax.persistence.*;
 import java.math.BigDecimal;
 
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @Data
@@ -23,4 +27,10 @@ public class Ingredient {
 
     @OneToOne(fetch = FetchType.EAGER)
     UnitOfMeasure unitOfMeasure;
+
+    public Ingredient(String description, BigDecimal amount, UnitOfMeasure unitOfMeasure) {
+        this.description = description;
+        this.amount = amount;
+        this.unitOfMeasure = unitOfMeasure;
+    }
 }
