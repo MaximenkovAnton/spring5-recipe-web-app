bafcb68a6198a20a03b9212f6687b45b43215e5e
Anton Maximenkov <maximenkovanton@gmail.com>
2018-04-04 13:12:41 +0300

    Added JPA Query findByDescription for categories

--- src/main/java/me/spring/mvc/recipe_project/repositories/CategoryRepository.java
+++ src/main/java/me/spring/mvc/recipe_project/repositories/CategoryRepository.java
@@ -3,5 +3,8 @@ package me.spring.mvc.recipe_project.repositories;
 import me.spring.mvc.recipe_project.domain.Category;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends CrudRepository<Category, Long>{
+    Optional<Category> findByDescription(String description);
 }
