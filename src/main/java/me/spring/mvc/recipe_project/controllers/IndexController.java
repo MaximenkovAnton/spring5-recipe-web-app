f969f937da089ba137a07c244c46206705cb6fdd
Anton Maximenkov <maximenkovanton@gmail.com>
2018-04-04 20:12:44 +0300

    Adding a table with recipes to index

--- src/main/java/me/spring/mvc/recipe_project/controllers/IndexController.java
+++ src/main/java/me/spring/mvc/recipe_project/controllers/IndexController.java
@@ -1,14 +1,21 @@
 package me.spring.mvc.recipe_project.controllers;
 
+import me.spring.mvc.recipe_project.services.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
+    private final RecipeService recipeService;
 
     @RequestMapping({"", "/", "index"})
     public String getIndexPage(Model model){
+        model.addAttribute("recipes", recipeService.getRecipies());
         return "index";
+    }
+
+    public IndexController(RecipeService recipeService) {
+        this.recipeService = recipeService;
     }
 }
