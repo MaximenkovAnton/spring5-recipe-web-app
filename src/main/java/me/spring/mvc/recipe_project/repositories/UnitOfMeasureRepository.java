c37a76e63252bb868e89513b6a1d1c17ebe532fd
Anton Maximenkov <maximenkovanton@gmail.com>
2018-04-04 13:12:12 +0300

    Added JPA Query findByUnit for unit of measure

--- src/main/java/me/spring/mvc/recipe_project/repositories/UnitOfMeasureRepository.java
+++ src/main/java/me/spring/mvc/recipe_project/repositories/UnitOfMeasureRepository.java
@@ -3,5 +3,8 @@ package me.spring.mvc.recipe_project.repositories;
 import me.spring.mvc.recipe_project.domain.UnitOfMeasure;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long>{
+    Optional<UnitOfMeasure> findByUnit(String unit);
 }
