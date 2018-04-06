7c9f63d2caedb64cc07365034a4f6235760a5c06
Anton Maximenkov <maximenkovanton@gmail.com>
2018-04-06 12:18:02 +0300

    Added unit of measure integration test

---
+++ src/test/java/me/spring/mvc/recipe_project/repositories/UnitOfMeasureRepositoryIT.java
@@ -0,0 +1,32 @@
+package me.spring.mvc.recipe_project.repositories;
+
+import me.spring.mvc.recipe_project.domain.UnitOfMeasure;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class UnitOfMeasureRepositoryIT {
+
+    @Autowired
+    private UnitOfMeasureRepository repository;
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void findByUnit() {
+        Optional<UnitOfMeasure> optionalUnit = repository.findByUnit("Teaspoon");
+
+        assertEquals(optionalUnit.get().getUnit(), "Teaspoon");
+    }
+}
