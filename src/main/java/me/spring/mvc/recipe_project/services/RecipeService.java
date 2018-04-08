dfa13f3fe44fc3934ead38bddc7031a1f9a881a1
Anton Maximenkov <maximenkovanton@gmail.com>
2018-04-08 03:39:09 +0300

    Added recipe view service and controller

--- src/main/java/me/spring/mvc/recipe_project/services/RecipeService.java
+++ src/main/java/me/spring/mvc/recipe_project/services/RecipeService.java
@@ -6,4 +6,5 @@ import java.util.Set;
 
 public interface RecipeService {
     Set<Recipe> getRecipies();
+    Recipe findById(Long id);
 }
--- src/main/java/me/spring/mvc/recipe_project/services/RecipeServiceImpl.java
+++ src/main/java/me/spring/mvc/recipe_project/services/RecipeServiceImpl.java
@@ -1,13 +1,16 @@
 package me.spring.mvc.recipe_project.services;
 
+import lombok.RequiredArgsConstructor;
 import me.spring.mvc.recipe_project.domain.Recipe;
 import me.spring.mvc.recipe_project.repositories.RecipeRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
 @Service
+@RequiredArgsConstructor
 public class RecipeServiceImpl implements RecipeService {
     private final RecipeRepository recipeRepository;
     @Override
@@ -17,7 +20,10 @@ public class RecipeServiceImpl implements RecipeService {
         return recipes;
     }
 
-    public RecipeServiceImpl(RecipeRepository recipeRepository) {
-        this.recipeRepository = recipeRepository;
+    @Override
+    public Recipe findById(Long id) {
+        Optional<Recipe> recipeOptional = recipeRepository.findById(id);
+        if(!recipeOptional.isPresent()) throw new RuntimeException("Recipe not found");
+        return recipeOptional.get();
     }
 }
